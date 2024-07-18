0e7211304142bab69a4031ef061373a990cc6a9e
Tan Yi Cherng <84616043+tanyc-2003@users.noreply.github.com>
2024-07-19 00:26:59 +0800

    base

---
+++ app.py
@@ -0,0 +1,140 @@
+from flask import Flask, render_template, request, jsonify, send_file
+from flask_cors import CORS
+
+import webbrowser
+import gunicorn
+import os
+import json
+from datetime import datetime
+import csv
+import io
+
+import firebase_admin
+from firebase_admin import credentials, storage, firestore
+
+import openai
+from dotenv import load_dotenv
+
+
+
+# Open the browser automatically (local)
+# url = "http://127.0.0.1:5000"
+# webbrowser.open(url)
+
+#Server configurations
+app = Flask(__name__, static_folder='static', template_folder='templates')
+app.config['UPLOAD_FOLDER'] = "Uploads"
+CORS(app)
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate('./iv-curve-tracer-web-firebase-adminsdk-3i0ne-8dd09a7259.json')
+firebase_admin.initialize_app(cred, {
+    'storageBucket': "iv-curve-tracer-web.appspot.com"
+})
+
+db = firestore.client()
+bucket = storage.bucket()
+
+
+# Variables
+current_selection = ''
+
+
+# App routes 
+@app.route('/')
+def mainPage():
+    return render_template('index.html')
+
+@app.route('/dataPacketAvailable', methods=["GET", "POST"])
+def dataPacketAvailable():
+    
+    dataPacket = request.get_json()
+    print(dataPacket)
+
+    global bucket
+    timeStamp = dateTime()
+    temp_csv_filename = 'temp_csv_file.csv'
+    csv_filename = timeStamp + '.csv'
+
+    # write to csv file -> convert from dictionary
+    with open(temp_csv_filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        for key, value in dataPacket.items():
+            writer.writerow([key, value])
+
+    print("csv created")
+
+    # upload to firebase storage
+    blob = bucket.blob(csv_filename)
+    blob.upload_from_filename(temp_csv_filename)
+
+    print("csv uploaded to firebase storage")
+
+    return 'Succeed'
+
+
+@app.route('/getDataPacket/<csv_filename>', methods=["GET", "POST"])
+def getDataPacket(csv_filename):
+    global bucket
+    global current_selection
+    current_selection = csv_filename
+
+    # get csv from firebase storage
+    blob = bucket.blob(csv_filename)
+    csv_content = blob.download_as_string()
+
+    # read csv file - convert to dictionary
+    dataPacket = {}
+    csv_file = io.StringIO(csv_content.decode('utf-8'))
+    reader = csv.reader(csv_file)
+    for row in reader:
+        key = float(row[0])
+        value = float(row[1])
+        dataPacket[key] = value
+
+    return jsonify(dataPacket)
+
+
+@app.route('/downloadCSV', methods=["GET", "POST"])
+def downloadCSV():
+    global bucket
+    global current_selection
+    csv_filename = current_selection
+    local_filename = 'downloaded_' + csv_filename
+
+    # get csv from firebase storage
+    blob = bucket.blob(csv_filename)
+    blob.download_to_filename(local_filename)
+
+    # Serve the file to the client
+    return send_file(local_filename, as_attachment=True)
+
+
+@app.route('/list_files', methods=['GET'])
+def list_files():
+    global bucket
+    blobs = bucket.list_blobs()
+    
+    filenames = [blob.name for blob in blobs]
+    # print(filenames)
+    
+    return jsonify(filenames)
+
+
+# Generate data and time 
+def dateTime():
+    # Get current date and time
+    current_datetime = datetime.utcnow()
+    # Format the date and time
+    formatted_datetime = current_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    return formatted_datetime
+
+
+
+
+if __name__ == "__main__":
+    # appFlask.run(ssl_context='adhoc')
+    app.run(ssl_context='adhoc')
+    # port = os.environ.get("PORT", 5000)# Get port number of env at runtime, else use default port 5000
+    # app.run(debug=True, host='0.0.0.0', port=port)  # 0.0.0.0 port forwarding resolves the host IP address at runtime
---
+++ static/assets/js/index.js
@@ -0,0 +1,153 @@
+
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+
+function download_CSV_btn() {
+  var url = "/downloadCSV";
+  
+  // Create a temporary link element
+  var link = document.createElement('a');
+  link.href = url;
+  link.download = ''; // The download attribute prompts the browser to download the file
+  link.style.display = 'none'; // Hide the link
+  
+  // Append the link to the body
+  document.body.appendChild(link);
+  
+  // Programmatically click the link
+  link.click();
+  
+  // Remove the link from the document
+  document.body.removeChild(link);
+}
+
+
+//-------------------- DATA ----------------------- //
+
+var IVData ={};
+var voltageData ={};
+var currentData ={};
+
+async function plotGraph(filename) {
+  const dataIDElements = document.getElementsByClassName('data-ID');
+  for (let i = 0; i < dataIDElements.length; i++) {
+    dataIDElements[i].textContent = "/" + filename;
+  }
+
+  try {
+    const url = '/getDataPacket/'+ (filename).replace(" ", "%20");
+    const IVDataResponse = await fetch(url);
+    IVData = await IVDataResponse.json();
+    // console.log('Load Demand Data:', IVData);
+
+    // Convert JSON object to array format required by ApexCharts
+    const formattedIVData = Object.entries(IVData).map(([voltage, current]) => {
+      return { x: parseFloat(voltage), y: current };
+    });
+
+    // Debug - Check the value 
+    console.log(filename);
+    console.log(formattedIVData); 
+      
+    // Plot graph
+    new ApexCharts(document.querySelector("#reportsChart"), {
+      chart: {
+        type: 'line',
+        height: 350,
+        zoom: {
+            enabled: true
+        }
+      },
+      series: [{
+          name: 'Current vs Voltage',
+          data: formattedIVData
+      }],
+      xaxis: {
+          title: {
+              text: 'Voltage (V)'
+          }
+      },
+      yaxis: {
+          title: {
+              text: 'Current (A)'
+          }
+      },
+      title: {
+          text: 'IV Curve of Solar Cell',
+          align: 'left'
+      }
+
+    }).render();
+      
+  } catch (error) {
+      console.error('Error fetching data:', error);
+  }
+
+  
+}
+
+
+// Funtion to append data from the JSON object into list
+function pushValueToList(jsonObject) {
+  var myList = [];
+  for (var key in jsonObject) {
+        myList.push(jsonObject[key]);
+  } 
+  console.log(myList);
+  return myList;
+}
+
+function pushKeyToList(jsonObject) {
+  var myList = [];
+  for (var key in jsonObject) {
+        myList.push(key);
+  }
+  console.log(myList);
+  return myList;
+}
+
+
+//-------------------- Fetch Files ----------------------- //
+
+// Fetch files when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+
+  // Function to fetch file names to frontend
+  async function fetchFiles() {
+    const response = await fetch('/list_files');
+    const filenames = await response.json();
+    console.log(filenames)
+
+    const sidebarNav = document.getElementById('sidebar-nav');
+
+    filenames.forEach(filename => {
+        const navItem = document.createElement('li');
+        navItem.classList.add('nav-item');
+
+        const navLink = document.createElement('a');
+        navLink.classList.add('nav-link');
+        navLink.addEventListener('click', function(event) {
+          event.preventDefault();
+          plotGraph(filename);
+        });
+
+        const span = document.createElement('span');
+        span.textContent = filename;
+        
+        navLink.appendChild(span);
+        navItem.appendChild(navLink);
+        sidebarNav.appendChild(navItem);
+
+    });
+  }
+
+  fetchFiles();
+
+});
